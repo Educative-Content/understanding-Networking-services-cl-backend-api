f30f7f0026962c42ca9941c7ab40a631958d0f01
Haziq Ali <60395311+theHaziqali@users.noreply.github.com>
2023-09-14 14:07:35 +0500

    Set-up

---
+++ simple-node-api/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+// Sample data - a list of books
+let books = [
+  { id: 1, title: 'Book 1', author: 'Author 1' },
+  { id: 2, title: 'Book 2', author: 'Author 2' },
+  { id: 3, title: 'Book 3', author: 'Author 3' }
+];
+
+// Route to get all books
+app.get('/api/books', (req, res) => {
+  res.json(books);
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
